26e04c7aad0ce43ca607c4199ad1ede319823f27
Michael Ernst <mernst@alum.mit.edu>
2024-01-29 06:00:10 -0800

    Get `git-clone-related` from git-scripts repository, not plume-scripts (#62)

--- run-cf-tests.sh
+++ run-cf-tests.sh
@@ -9,17 +9,17 @@ set -o pipefail
 ### dljc (i.e. this project's main script). It is intended to be used as part of a CI integration test.
 ### This script must be run from the top-level directory
 
-### get plume-scripts so that we can use git-clone-related
+### get git-scripts so that we can use git-clone-related
 
-if [ -d /tmp/"$USER"/plume-scripts ] ; then
-  git -C /tmp/"$USER"/plume-scripts pull -q > /dev/null 2>&1
+if [ -d /tmp/"$USER"/git-scripts ] ; then
+  git -C /tmp/"$USER"/git-scripts pull -q > /dev/null 2>&1
 else
-  mkdir -p /tmp/"$USER" && git -C /tmp/"$USER" clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
+  mkdir -p /tmp/"$USER" && git -C /tmp/"$USER" clone --depth 1 -q https://github.com/plume-lib/git-scripts.git
 fi
 
 ### Clone the CF. If there is a branch with the same name as this branch, git-clone-related will check out that branch.
 rm -rf /tmp/"$USER"/checker-framework
-/tmp/"$USER"/plume-scripts/git-clone-related typetools checker-framework /tmp/"$USER"/checker-framework
+/tmp/"$USER"/git-scripts/git-clone-related typetools checker-framework /tmp/"$USER"/checker-framework
 export CHECKERFRAMEWORK=/tmp/"$USER"/checker-framework
 
 export DLJC=$(pwd)/dljc
